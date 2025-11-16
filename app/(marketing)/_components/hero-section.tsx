52fd3fb3a32a214ad37894004e50f90f4a1e454f
sakilalakmal <sakilalakmal77@gmail.com>
2025-11-16 09:35:03 +0530

    made hero section changes

---
+++ app/(marketing)/_components/hero-section.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { HeroHeader } from "./header";
+import { InfiniteSlider } from "@/components/ui/infinite-slider";
+import { ProgressiveBlur } from "@/components/ui/progressive-blur";
+import bgImage from "@/public/hero.png";
+import arcjet from "@/public/arcjet.png";
+import orpc from "@/public/orpc.webp";
+import kind from "@/public/kinde.png";
+import nike from "@/public/bluh.png";
+import neon from "@/public/neon.png";
+
+export default function HeroSection() {
+  return (
+    <>
+      <HeroHeader />
+      <main className="overflow-x-hidden">
+        <section>
+          <div className="pb-24 pt-12 md:pb-32 lg:pb-56 lg:pt-44">
+            <div className="relative mx-auto max-w-6xl px-6">
+              <div className="flex flex-col items-center lg:flex-row lg:items-center lg:justify-between">
+                <div className="mx-auto max-w-lg text-center lg:mx-0 lg:w-1/2 lg:text-left lg:pr-8">
+                  <h1 className="mt-8 max-w-2xl text-balance text-5xl font-medium md:text-6xl lg:mt-0 xl:text-7xl">
+                    Ship 10x Faster with Nexus
+                  </h1>
+                  <p className="mt-8 max-w-2xl text-pretty text-lg">
+                    The AI ready home for team collaboration. Nexus organizes
+                    conversation into channel with threads in realtime
+                  </p>
+
+                  <div className="mt-12 flex flex-col items-center justify-center gap-6 sm:flex-row lg:justify-start">
+                    <Button asChild size="lg" className="px-5 text-base">
+                      <Link href="#link">
+                        <span className="text-nowrap">Get Started</span>
+                      </Link>
+                    </Button>
+                    <Button
+                      key={2}
+                      asChild
+                      size="lg"
+                      variant="secondary"
+                      className="px-5 text-base"
+                    >
+                      <Link href="#link">
+                        <span className="text-nowrap">Request a demo</span>
+                      </Link>
+                    </Button>
+                  </div>
+                </div>
+                <div className="mt-12 lg:mt-0 lg:w-1/2 lg:flex lg:justify-center">
+                  <Image
+                    className="h-56 w-full object-cover invert sm:h-96 lg:h-80 lg:w-auto lg:max-w-md lg:object-contain dark:mix-blend-lighten dark:invert-0"
+                    src={bgImage}
+                    alt="Abstract Object"
+                    height="4000"
+                    width="1000"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="bg-background pb-16 md:pb-32">
+          <div className="group relative m-auto max-w-6xl px-6">
+            <div className="flex flex-col items-center md:flex-row">
+              <div className="md:max-w-44 md:border-r md:pr-6">
+                <p className="text-end text-sm">Powering the best teams</p>
+              </div>
+              <div className="relative py-6 md:w-[calc(100%-11rem)]">
+                <InfiniteSlider speedOnHover={20} speed={40} gap={112}>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-5 w-fit dark:invert"
+                      src={"https://html.tailus.io/blocks/customers/nvidia.svg"}
+                      alt="Nvidia Logo"
+                      height="20"
+                      width="auto"
+                    />
+                  </div>
+
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-4 w-fit dark:invert"
+                      src="https://html.tailus.io/blocks/customers/column.svg"
+                      alt="Column Logo"
+                      height="16"
+                      width="auto"
+                    />
+                  </div>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-4 w-fit dark:invert"
+                      src="https://html.tailus.io/blocks/customers/github.svg"
+                      alt="GitHub Logo"
+                      height="16"
+                      width="auto"
+                    />
+                  </div>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-5 w-fit dark:invert"
+                      src="https://html.tailus.io/blocks/customers/nike.svg"
+                      alt="Nike Logo"
+                      height="20"
+                      width="auto"
+                    />
+                  </div>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-5 w-fit dark:invert"
+                      src="https://html.tailus.io/blocks/customers/lemonsqueezy.svg"
+                      alt="Lemon Squeezy Logo"
+                      height="20"
+                      width="auto"
+                    />
+                  </div>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-4 w-fit dark:invert"
+                      src="https://html.tailus.io/blocks/customers/laravel.svg"
+                      alt="Laravel Logo"
+                      height="16"
+                      width="auto"
+                    />
+                  </div>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-7 w-fit dark:invert"
+                      src="https://html.tailus.io/blocks/customers/lilly.svg"
+                      alt="Lilly Logo"
+                      height="28"
+                      width="auto"
+                    />
+                  </div>
+
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-6 w-fit dark:invert"
+                      src="https://html.tailus.io/blocks/customers/openai.svg"
+                      alt="OpenAI Logo"
+                      height="24"
+                      width="auto"
+                    />
+                  </div>
+                </InfiniteSlider>
+
+                <div className="bg-linear-to-r from-background absolute inset-y-0 left-0 w-20"></div>
+                <div className="bg-linear-to-l from-background absolute inset-y-0 right-0 w-20"></div>
+                <ProgressiveBlur
+                  className="pointer-events-none absolute left-0 top-0 h-full w-20"
+                  direction="left"
+                  blurIntensity={1}
+                />
+                <ProgressiveBlur
+                  className="pointer-events-none absolute right-0 top-0 h-full w-20"
+                  direction="right"
+                  blurIntensity={1}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
